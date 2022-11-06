936983141b2e416afbae3adf3bbfb728fcdc8e06
Tozé Mandela <mandelajunior10@gmail.com>
2022-11-06 21:39:02 +0100

    remove nome do conversor Ymate

---
+++ main.js
@@ -0,0 +1,22 @@
+const fs = require('fs').promises;
+const path =  require('path');
+
+/* let input = document.getElementById('caminho');
+let btn = document.querySelector('button'); */
+
+const caminho = 'C:/Users/Djiny Depp/Downloads';
+let a = fs.readdir( caminho);
+
+a.then((d)=>{
+    d.forEach((el)=>{
+        if(el.match(/\.mp3$/)){
+            const newName =  el.replace(/(y2mate\.com\s-\s)/gi,'');
+
+             fs.rename(`${caminho}/${el}` , `${caminho}/${newName}`).then(()=>{
+                console.log('renomeado com sucesso...!');
+             }).catch(e=>{
+                console.error(e)
+             });
+            }
+    })
+})
---
+++ main2.js
@@ -0,0 +1,36 @@
+const fs = require('fs').promises;
+const path =  require('path');
+const caminho1 = 'C:/Users/Djiny Depp/Desktop/Nova pasta (2)';
+
+ receiveData(caminho1);
+
+function receiveData(caminho){
+    fs.readdir(caminho).then(data=>{
+
+        cleanData(data);
+
+    }).catch(e=>console.log('Erro ao receber os dados'));
+}
+
+function cleanData(data){
+    let requiredData = new Array();
+    data.forEach((el)=>{
+        if(el.match(/\.mp3$/)){
+            requiredData.push(el);
+        }
+    });
+    renameFile(requiredData, caminho1);
+}
+
+function renameFile(data, caminho){
+    data.forEach(el=>{
+        const newName =  el.replace(/(y2mate\.com\s-\s)/gi,'');
+
+        fs.rename(`${caminho}/${el}` , `${caminho}/${newName}`).then(()=>{
+            console.log('renomeado com sucesso...!');
+        }).catch(e=>{
+            console.error(e)
+        });
+
+    })
+}
